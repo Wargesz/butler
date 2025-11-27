8639e3f39f0b96cc6565c9e78ddb328ebef339f9
Varga Marcell <computing8063@mailbox.unideb.hu>
2025-11-27 17:54:14 +0100

    added nested folder style update. updated click event to match new dom

--- static/vault.js
+++ static/vault.js
@@ -1,22 +1,38 @@
+registerClickEvents();
+indentNestedFolders();
+
 function registerClickEvents() {
-	for (const e of document.querySelectorAll('.pocket')) {
+	for (const e of document.querySelectorAll('.file')) {
 		e.addEventListener('click', async () => {
 			path = getPathOfElement(e);
-			const r = await fetch(`file?path=${path}`);
+			const r = await fetch(`file?path=${path}/${e.innerText}`);
 			const text = await r.text();
 			const element = document.querySelector('#viewer');
 			element.innerText = text;
-            element.style.display = 'block';
+			element.style.display = 'block';
 		});
 	}
 }
 
-registerClickEvents();
+function indentNestedFolders() {
+	for (const e of document.querySelectorAll('.nested')) {
+		const level = getNestedLevel(e);
+		e.style.marginLeft = `${level}em`;
+	}
+}
 
 function getPathOfElement(e) {
 	for (const c of e.classList) {
 		if (c.startsWith('path-')) {
 			return c.split('path-')[1];
+		}
+	}
+}
+
+function getNestedLevel(e) {
+	for (const c of e.classList) {
+		if (c.startsWith('nested-')) {
+			return Number.parseInt(c.split('nested-')[1]);
 		}
 	}
 }
