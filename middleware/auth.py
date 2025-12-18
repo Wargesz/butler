8210fc998b94ef1b1207fbbc82d7064e6f716c51
Varga Marcell <computing8063@mailbox.unideb.hu>
2025-12-18 19:58:48 +0100

    updated to use url_for

--- middleware/auth.py
+++ middleware/auth.py
@@ -1,4 +1,4 @@
-from flask import request, redirect, session
+from flask import request, redirect, session, url_for
 from dotenv import dotenv_values
 from jwt import decode, ExpiredSignatureError
 from models.models import User
@@ -10,7 +10,7 @@ def auth(f):
     def wrap(*args, **kwargs):
         token = request.cookies.get('Authorize')
         if token is None:
-            return redirect('/login')
+            return redirect(url_for('root.get_login'))
         try:
             claims = decode(token, env['SECRET'], algorithms=["HS256"])
             user = User.query.filter(User.id == claims['sub']).first()
