fbd84ee078ce7c09cc091b875d2a4e3ed4c6ed5a
Varga Marcell <computing8063@mailbox.unideb.hu>
2025-12-18 20:03:00 +0100

    fix: use url_for on redirects

--- routes/root_routes.py
+++ routes/root_routes.py
@@ -1,6 +1,6 @@
 from flask import (
         Blueprint, render_template, request, redirect, session,
-        send_from_directory)
+        send_from_directory, url_for)
 from dotenv import dotenv_values
 from random import choice
 from controllers.db import DB
@@ -76,7 +76,7 @@ def post_login():
         return render_template('login.html', error_message='invalid username')
     if bcrypt.checkpw(password.encode('utf-8'),
                       user.password.encode('utf-8')):
-        res = redirect('/')
+        res = redirect(url_for('root.root'))
         cookie, exp = signCookie(user)
         res.set_cookie('Authorize', cookie, expires=exp, httponly=True)
         return res
@@ -108,7 +108,7 @@ def post_register():
     DB.add(user)
     DB.commit()
     createContentDir(user)
-    res = redirect('/')
+    res = redirect(url_for('root.root'))
     cookie, exp = signCookie(user)
     res.set_cookie('Authorize', cookie, expires=exp, httponly=True)
     return res
