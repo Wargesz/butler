22e6ce1c4fbfacc4d2f6ef945eb7401b4a5d29ff
Varga Marcell <computing8063@mailbox.unideb.hu>
2025-12-22 01:57:17 +0100

    added statements with one prepared statement to read top 10 projects of user

---
+++ controllers/statements.py
@@ -0,0 +1,26 @@
+from models.models import User
+from sqlalchemy.sql import text
+from controllers.db import DB
+
+stmt = {}
+
+stmt['GET_USER_ACTIVITY'] = """
+SELECT substr(replace(edited_file, :p, ""), 0,
+        instr(replace(edited_file, :p, ""), "/")) AS project,
+        sum(seconds) AS total_time
+        FROM midnight
+        WHERE user_id == (SELECT id FROM user WHERE username == :u)
+        GROUP BY 1
+        ORDER BY 2 DESC
+        LIMIT 10;
+"""
+
+
+def getUserActivity(username):
+    user = User.query.filter(User.username == username).first()
+    return eval(stmt['GET_USER_ACTIVITY'], {'p': user.project_folder,
+                                            'u': user.username})
+
+
+def eval(stmt, p):
+    return DB.execute(text(stmt), p).all()
