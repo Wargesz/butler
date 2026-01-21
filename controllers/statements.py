32ccfdc68f19a220287dc766c356b9d638db1a19
Varga Marcell <computing8063@mailbox.unideb.hu>
2026-01-21 21:05:42 +0100

    added project activity statement

--- controllers/statements.py
+++ controllers/statements.py
@@ -15,11 +15,28 @@ SELECT substr(replace(edited_file, :p, ""), 0,
         LIMIT 10;
 """
 
+stmt['GET_PROJECT_ACTIVITY'] = """
+SELECT substr(edited_file, length(concat(:p, "/", :f)) + 1) as file,
+sum(seconds) AS total_time
+FROM midnight
+WHERE user_id == (SELECT id FROM user WHERE username == :u)
+AND instr(edited_file, :f)
+GROUP BY 1
+ORDER BY 2 DESC;
+"""
+
 
 def getUserActivity(username):
     user = User.query.filter(User.username == username).first()
     return eval(stmt['GET_USER_ACTIVITY'], {'p': user.project_folder,
                                             'u': user.username})
+
+
+def getProjectActivity(username, projectname):
+    user = User.query.filter(User.username == username).first()
+    return eval(stmt['GET_PROJECT_ACTIVITY'], {'p': user.project_folder,
+                                               'u': user.username,
+                                               'f': projectname})
 
 
 def eval(stmt, p):
